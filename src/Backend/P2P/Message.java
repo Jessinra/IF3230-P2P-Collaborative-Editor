d0d2a66e86d2c8a4e7524d5aa400f5669fd1de0a
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-04-24 06:19:58 +0700

    Added P2P

---
+++ src/Backend/P2P/Message.java
@@ -0,0 +1,75 @@
+package Backend.P2P;
+
+
+/**
+ * A Plain Old Java Object (POJO) for facilitating serializing and de-serializing a message object.
+ */
+
+public class Message {
+    /**
+     * The ID of the originator of the message
+     */
+    private String senderId;
+
+    /**
+     * The ID of the intended recipient
+     */
+    private String destinationId;
+
+    /**
+     * The actual message to be delivered
+     */
+    private Object message;
+
+    /**
+     * No-argument constructor required for serialization and deserialization operations
+     */
+    public Message() {
+    }
+
+
+    /**
+     * Creates a message object
+     * @param senderId The originating node's ID
+     * @param destinationId The destination node's ID
+     * @param message The message content
+     */
+    public Message(String senderId, String destinationId, Object message) {
+        this.senderId = senderId;
+        this.destinationId = destinationId;
+        this.message = message;
+    }
+
+    /**
+     * Gets the sending node's ID
+     * @return
+     */
+    public String getSenderId() {
+        return senderId;
+    }
+
+    /**
+     * Gets the message object
+     * @return The message
+     */
+    public Object getMessage() {
+        return message;
+    }
+
+    /**
+     * The node ID of the receiver
+     * @return Node ID
+     */
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "senderId='" + senderId + '\'' +
+                ", destinationId='" + destinationId + '\'' +
+                ", message=" + message +
+                '}';
+    }
+}
---
+++ src/Backend/P2P/Node.java
@@ -0,0 +1,90 @@
+package Backend.P2P
+
+/**
+ * A node in a peer to peer network.
+ * It composes two main sub-components: inBound and outBound.
+ * It also implements the message callback interface to enable notification of incoming messages.
+ */
+public class Node implements IMessageCallback {
+    /**
+     * The inBound sub-component
+     */
+    private InBound inBound;
+
+    /**
+     * The outBound sub-component
+     */
+    private OutBound outBound;
+
+    /**
+     * The identification of a node in the network
+     */
+    private final String nodeId;
+
+    /**
+     * The port for listening for in-coming messages
+     */
+    private final int inBoundPort;
+
+    /**
+     * Instantiates a node object
+     * @param inBoundPort The listening port
+     * @param nodeId The identity of the node
+     */
+    public Node(int inBoundPort, String nodeId) {
+        this.inBound = new InBound(inBoundPort, this);
+        this.outBound = new OutBound();
+        this.nodeId = nodeId;
+        this.inBoundPort = inBoundPort;
+    }
+
+    /**
+     * Puts the node in online mode.
+     */
+    public void start() {
+        this.inBound.open();
+    }
+
+    /**
+     * Puts the node in an offline mode
+     */
+    public void stop() {
+        this.inBound.setIsOnline(false);
+    }
+
+    /**
+     * Sends a message to a destination
+     * @param msg The message to be sent
+     * @param dest_addr The destination address (e.g. IPv4 address)
+     * @param port The destination port number
+     */
+    public void sendMessage(Message msg, String dest_addr, int port) {
+        System.out.println(nodeId + " is sending message to " + msg.getDestinationId());
+        this.outBound.send(dest_addr, port, msg.toString());
+    }
+
+    /**
+     * Gets the ID of the node
+     * @return The node ID
+     */
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    /**
+     * Gets the listening port number
+     * @return The port number
+     */
+    public int getInBoundPort() {
+        return inBoundPort;
+    }
+
+    @Override
+    public void messageReceived(String msg) {
+        String m = msg;
+        System.out.println(nodeId + " has received a msg: " + m);
+        if(m != null) {
+
+        }
+    }
+}
